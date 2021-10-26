8b07c9b66359df81c4f7ad581ccbbe3f5e844179
ylebovits <yakirlebovits@gmail.com>
2021-10-26 13:23:45 -0400

    Initial SpringBoot commit

---
+++ src/test/java/com/cis/rockon/RockOnCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cis.rockon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockOnCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
